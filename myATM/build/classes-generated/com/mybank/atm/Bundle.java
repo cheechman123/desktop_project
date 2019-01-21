3bf12aa9026be3c4791ab4bfa2b04e6b988e653a
Денис <cheechman123@gmail.com>
2019-01-21 12:57:28 +0200

    add doc

---
+++ myATM/build/classes-generated/com/mybank/atm/Bundle.java
@@ -0,0 +1,27 @@
+package com.mybank.atm;
+/** Localizable strings for {@link com.mybank.atm}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>atm</i>
+     * @see atmTopComponent
+     */
+    static String CTL_atmAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_atmAction");
+    }
+    /**
+     * @return <i>atm Window</i>
+     * @see atmTopComponent
+     */
+    static String CTL_atmTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_atmTopComponent");
+    }
+    /**
+     * @return <i>This is a atm window</i>
+     * @see atmTopComponent
+     */
+    static String HINT_atmTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_atmTopComponent");
+    }
+    private void Bundle() {}
+}
